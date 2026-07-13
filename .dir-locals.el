12bfe46b53fffb2489d6c6439ee9771a39371bbb
Gaelan D'costa <gaelan@fastmail.ca>
2026-07-13 17:15:54 -0400

    fix: add project directory to post-save testing hook
    Populate load-path so that (require) can locate project files

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,9 +1,10 @@
 ((emacs-lisp-mode . ((eval . (progn
 			       (defun tarot-emacs/test ()
 				 (require 'ert)
-				 (load-file "tarot.el")
-				 (load-file "tarot-meanings.el")
-				 (load-file "tarot-test.el")
+				 (add-to-list 'load-path default-directory)
+				 (load-file "./tarot-meanings.el")
+				 (load-file "./tarot.el")
+				 (load-file "./tarot-test.el")
 				 (save-selected-window
 				   (ert-run-tests-interactively t)))
 			       (add-hook 'after-save-hook #'tarot-emacs/test nil t))))))
